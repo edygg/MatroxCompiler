652c69d8a756c5fcf16d3baa4d5921242bb0177c
Guillermo Mazzoni <mazzoni2092@gmai.com>
2015-03-23 18:33:32 -0600

    Changes in If

--- src/edu/unitec/ast/If.java
+++ src/edu/unitec/ast/If.java
@@ -6,11 +6,20 @@ import edu.unitec.visitor.TypeVisitor;
 public class If extends Statement {
   public Exp e;
   public Statements s1,s2;
+  public ElseIfStatements eis;
 
   public If(Exp ae, Statements as1, Statements as2) {
     e=ae; s1=as1; s2=as2;
   }
 
+  public If(Exp ae, Statements as1, ElseIfStatements aeis) {
+    e=ae; s1=as1; eis=aeis;
+  }
+  
+  public If(Exp ae, Statements as1, ElseIfStatements aeis ,Statements as2) {
+    e=ae; s1=as1; eis=aeis; s2=as2;
+  }
+  
   public void accept(Visitor v) {
     v.visit(this);
   }
