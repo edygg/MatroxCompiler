52374e9ffe380797f63724d35ce6c49f71966265
Edilson Gonzalez <edilson.gonza@unitec.edu>
2015-05-19 21:35:02 -0600

    Semantic analysis

--- src/edu/unitec/matrox/SemanticAnalysis.java
+++ src/edu/unitec/matrox/SemanticAnalysis.java
@@ -1,6 +1,7 @@
 
 package edu.unitec.matrox;
 
+import java.util.Hashtable;
 import java.util.Objects;
 
 /**
@@ -9,4 +10,25 @@ import java.util.Objects;
  */
 public class SemanticAnalysis {
     
+    private Hashtable<String, SemanticTableNode> semanticTable;
+
+    public SemanticAnalysis() {
+        this.semanticTable = new Hashtable<String, SemanticTableNode>();
+    }
+    
+    public SemanticTableNode findID(String id) {
+        return this.semanticTable.get(id);
+    }
+    
+    public boolean addID(String id, SemanticTableNode value) {
+        if (findID(id) != null)
+            return false;
+        this.semanticTable.put(id, value);
+        return true;
+    }
+    
+    public Hashtable<String, SemanticTableNode> getSemanticTable() {
+        return semanticTable;
+    }
+
 }
